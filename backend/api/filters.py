b58ad6886792665413d516ff0b8b0ff2f7e578a0
Ilian Lypota <ilianlypota@gmail.com>
2023-04-05 15:02:12 +0300

    filter change

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 from rest_framework import filters as f
 
 
@@ -24,7 +24,11 @@ class CustomRecipeFilter(FilterSet):
     Filter example:
     /api/recipes/?tags=lunch&author=10&is_in_shopping_cart=0&is_favorited=1
     """
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
