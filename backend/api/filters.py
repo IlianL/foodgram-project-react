ccecb31821be176d327850eafcba96253cc2aa7a
Ilian Lypota <ilianlypota@gmail.com>
2023-04-05 14:11:37 +0300

    filter fix

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import FilterSet, filters
 from recipes.models import Ingredient, Recipe
 from rest_framework import filters as f
+from users.models import User
 
 
 class CustomIngredientsFilter(f.SearchFilter):
@@ -17,13 +18,14 @@ class CustomIngredientsFilter(f.SearchFilter):
 
 class CustomRecipeFilter(FilterSet):
     """Фильтр для рецептов.
-    author (int) = фильтрация по полю id
-    tags (int) = фильтрация по полю id
+    author (str) = фильтрация по полю username
+    tags (str) = фильтрация по полю slug
     is_favorited (bool) = булевое значение 1/0
     is_in_shopping_cart (bool) = булевое значение 1/0
     Filter example:
-    /api/recipes/?tags=4&author=10&is_in_shopping_cart=0&is_favorited=1
+    /api/recipes/?tags=lunch&author=10&is_in_shopping_cart=0&is_favorited=1
     """
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
